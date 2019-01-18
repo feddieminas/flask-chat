81d40e510405154eb22e75fbce2619445fa56ede
Faidon Dermesonoglou <faidon.dermesonoglou@gmail.com>
2019-01-18 16:40:32 +0000

    Implement basic views and routing

--- run.py
+++ run.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello There!</h1>"
+    """Main page with instructions"""
+    return "To send a message use /USERNAME/MESSAGE" # want is to have it a s a URL www.flask/aaron/hello there
+    #return "<h1>Hello There!</h1>"
+    
+@app.route('/<username>') 
+def user(username):
+    return "Hi " + username
+    
+@app.route('/<username>/<message>')
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
     
 app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True) 
 
